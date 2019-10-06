d9349e4f3a285b4ae93c1ed13b8e19a60c5a7d1b
akels <akels14@gmail.com>
2019-10-06 19:33:32 +0200

    deprecation fixes

--- src/Adaptive.jl
+++ src/Adaptive.jl
@@ -12,37 +12,36 @@ import Base.getproperty
 import TaskMaster: Learner, ask!, tell! 
 
 abstract type AdaptiveLearner <: Learner end
-ask!(learner::AdaptiveLearner) = learner.learner[:ask](1)[1][1]
-tell!(learner::AdaptiveLearner,(xi,yi)) = learner.learner[:tell](xi,yi)
+ask!(learner::AdaptiveLearner,input) = learner.learner.ask(1)[1][1] ### Need to make ask! accept a state.
+tell!(learner::AdaptiveLearner,(xi,yi)) = learner.learner.tell(xi,yi)
 
 ######### 1D #########
 
 struct AdaptiveLearner1D <: AdaptiveLearner
     learner
 end
-AdaptiveLearner1D(box::Tuple{Number,Number}) = AdaptiveLearner1D(adaptive[:Learner1D](x->NaN,box))
+AdaptiveLearner1D(box::Tuple{Number,Number}) = AdaptiveLearner1D(adaptive.Learner1D(x->NaN,box))
 
 function Base.getproperty(l::AdaptiveLearner1D,s::Symbol)
     learner = getfield(l,:learner)
-    if s==:loss
-        learner[:loss]()
-    elseif s==:x
-        d = [(key,value) for (key,value) in learner[:data]]
+
+    if s==:x
+        d = [(key,value) for (key,value) in learner.data]
         x = Float64[i[1] for i in d]
         sort(x)
     elseif s==:y
-        d = [(key,value) for (key,value) in learner[:data]]
+        d = [(key,value) for (key,value) in learner.data]
         x = Float64[i[1] for i in d]
         y = Float64[i[2] for i in d]
         y[sortperm(x)]
     elseif s==:data
-        learner[:data]
-    elseif s==:pending
-        Set(p for p in learner[:pending_points])        
+        learner.data
+    elseif s==:pending_points
+        Set(p for p in learner.pending_points)        
     elseif s==:learner
         learner
     else
-        error("No such field defined")
+        getproperty(learner,s)
     end
 end
 
@@ -50,31 +49,26 @@ end
 
 struct AdaptiveLearner2D <: AdaptiveLearner
     learner
-    AdaptiveLearner2D(box) = new(adaptive[:Learner2D](x->NaN,box))
+    AdaptiveLearner2D(box) = new(adaptive.Learner2D(x->NaN,box))
 end
 
 function Base.getproperty(l::AdaptiveLearner2D,s::Symbol)
     learner = getfield(l,:learner)
-    if s==:loss
-        learner[:loss]()
-    elseif s==:ip # Does not change a state, but rather represents it
-        learner[:ip]()
-    elseif s==:points
-        tri = learner[:ip]()[:tri]
-        tri[:points]
+
+    if s==:points
+        tri = learner.ip().tri
+        tri.points
     elseif s==:vertices
-        tri = learner[:ip]()[:tri]
-        tri[:vertices] .+ 1
-    elseif s==:data
-        learner[:data]
-    elseif s==:pending
-        Set(p for p in learner[:pending_points])        
+        tri = learner.ip().tri
+        tri.vertices .+ 1
+    elseif s==:pending_points
+        Set(p for p in learner.pending_points)        
     elseif s==:values
         [v for v in py"$(learner).data.values()"]
     elseif s==:learner
         learner
     else
-        error("No such field defined")
+        getproperty(learner,s)
     end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,12 @@
-using Revise
+#using Revise
 
 using Adaptive
 using Distributed
-using TaskMaster
 
 @info "Testing ask! and tell! methods"
 function simpledriver!(f, learner,step)
-    while !(learner.loss < step)
-        xi = ask!(learner)
+    while !(learner.loss() < step)
+        xi = ask!(learner,true)
         yi = f(xi) 
         tell!(learner,(xi,yi))
     end
@@ -19,39 +18,7 @@ simpledriver!(x->exp(-x^2),learner1d,0.1)
 learner2d = AdaptiveLearner2D([(0,1),(0,1)])
 simpledriver!(x->exp(-x[1]^2 - x[2]^2),learner2d,0.1)
 
-@info "Now testing parallel exection with TaskMaster"
 
-@everywhere f(x) = exp(-x^2)
-learner1d = AdaptiveLearner1D((0,1))
-for (i,(xi,yi)) in enumerate(Master(f,learner1d))
-    @show (xi,yi)
-    if i==10
-        # In practice this way of breaking might be udersirable because
-        # some values are still being evaluated at this point
-        break
-    end
-end
-
-@info "Testing parallel exection with TaskMaster but with a soft exit"
-
-@everywhere f(x) = exp(-x^2)
-learner1d = AdaptiveLearner1D((0,1))
-wlearner1d = WrappedLearner(learner1d,learner->learner.loss<0.1)
-for (xi,yi) in Master(f,wlearner1d)
-    @show (xi,yi)
-end
-
-@info "Testing evaluate method"
-
-@everywhere f(x) = exp(-x^2)
-learner1d = AdaptiveLearner1D((0,1))
-evaluate(f,learner1d,learner->learner.loss<0.1)
-
-@info "Testing evaluate method with AdaptiveLearner2D"
-
-@everywhere f(x) = exp(-x[1]^2 - x[2]^2)
-learner2d = AdaptiveLearner2D([(0,1),(0,1)])
-evaluate(f,learner2d,learner->learner.loss<0.1)
 
 
 @info "Testing atribute access"
